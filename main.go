34717bac4ee9328cdc850622bb08c4f93de0f65f
Nicolas Pires <112504882+NicolasPires777@users.noreply.github.com>
2024-07-15 11:35:06 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/PuerkitoBio/goquery"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	// "time"
+
+	"github.com/PuerkitoBio/goquery"
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Artigo struct {
+    Titulo string `bson:"title"`
+    Noticia string `bson:"news"`
+    Data string `bson:"date"`
+    Categoria string `bson:"categoria"`
+}
+
 func main() {
+
+    mongodbconf := options.Client().ApplyURI("mongodb://localhost:27017")
+    client, err := mongo.Connect(context.TODO(),mongodbconf)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.Ping(context.TODO(),nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Print("Conectado ao Banco")
+
+    colecao := client.Database("Nicolas").Collection("news")
+
+
     url := "https://g1.globo.com"
 
     res, err := http.Get(url)
@@ -18,7 +49,15 @@ func main() {
         fmt.Print(err)
     }
     doc.Find(".feed-post-body").Each(func(index int, item *goquery.Selection) {
-        title := item.Text()
-        fmt.Printf("Artigo #%d: %s\n", index+1, title)
+        var article Artigo
+        article.Titulo = item.Find(".feed-post-header-chapeu").Text()
+        article.Noticia = item.Find("p").First().Text()
+        article.Data = item.Find(".feed-post-datetime").First().Text()
+        article.Categoria = item.Find(".feed-post-metadata-section").Text()
+
+        _, err := colecao.InsertOne(context.TODO(),article)
+        if err != nil {
+            log.Fatal(err)
+        }
     })
 }
